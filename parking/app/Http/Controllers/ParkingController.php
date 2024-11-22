9478a220186a4e5a0983ad6a63e45ea223bfea18
keshav <keshav.acewebx@gmail.com>
2024-11-23 00:51:16 +0530

    add laravel project

---
+++ parking/app/Http/Controllers/ParkingController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class ParkingController extends Controller
+{
+    public function index(){
+    
+    
+
+        return view('index');
+
+    }
+}
---
+++ parking/resources/views/index.php
@@ -0,0 +1,16 @@
+<?php 
+include('header.php');
+?>
+
+<div class="container">
+  <h3>Right Aligned Navbar</h3>
+  <p>The .navbar-right class is used to right-align navigation bar buttons.</p>
+</div>
+
+
+
+
+
+<?php 
+include('footer.php');
+?>
